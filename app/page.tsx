5acecb702df9c06c790336a7b9dd26611fe63024
adexterity <opensesame4me@gmail.com>
2023-11-04 22:54:24 +0100

    fetching data from the rapidAPI and displaying it on the page. created a utility function for API request and also for calculating car rent.

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,10 @@
-import { CustomFilter, Hero, SearchBar } from "@/components";
+import { CarCard, CustomFilter, Hero, SearchBar } from "@/components";
+import { fetchCars } from "@/utils";
 
-export default function Home() {
+export default async function Home() {
+  const allCars = await fetchCars();
+  const isDataEmpty = !Array.isArray(allCars) || allCars.length < 1 || !allCars;
+  console.log(allCars);
   return (
     <main className="overflow-hidden">
       <Hero />
@@ -12,10 +16,22 @@ export default function Home() {
         <div className="home__filters">
           <SearchBar />
           <div className="home__filter-container">
-            <CustomFilter title='fuel' />
-            <CustomFilter title='year' />
+            <CustomFilter title="fuel" />
+            <CustomFilter title="year" />
           </div>
         </div>
+        {!isDataEmpty ? (
+          <section>
+            <div className="home__cars-wrapper">
+              {allCars?.map((car)=>(<CarCard car={car} />))}
+            </div>
+          </section>
+        ) : (
+          <div className="home__error-container">
+            <h2 className="text-black text-xl font-bold">Ooops, no results</h2>
+            <p>{allCars?.message}</p>
+          </div>
+        )}
       </div>
     </main>
   );
